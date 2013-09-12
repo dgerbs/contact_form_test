233feae66584091f626b3cdf4a15f75a09e78935
Doug Gerber <dojoh@outlook.com>
2013-09-12 15:46:45 -0400

    Edit End Syntax

--- spec/features/admin_features_spec.rb
+++ spec/features/admin_features_spec.rb
@@ -17,4 +17,23 @@ So that I can respond or take action
 		expect(page).to have_content(field.email)
 		expect(page).to have_content(field.message)
 	end
+
+	feature "Contact Form", %q{
+As an admin
+I want to delete an inquiry
+So that I can remove spam or other undesirable inquiries
+} do
+
+# Acceptance Criteria:
+# I can remove an item from the listing of contact inquiries
+
+		it "should list messages for review" do
+			field = FactoryGirl.create(:contact_form)
+			visit '/contact_forms'
+			expect(page).to have_content(field.name)
+			expect(page).to have_content(field.subject)
+			expect(page).to have_content(field.email)
+			expect(page).to have_content(field.message)
+		end
+	end
 end
