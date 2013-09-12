0674f28d8cb5d96c21200959e848216c6da34b30
Doug Gerber <dojoh@outlook.com>
2013-09-12 14:47:26 -0400

    Add Capybara tests

---
+++ spec/features/user_features_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature "Contact Form", %q{
+# As a site visitor
+# I want to contact DogHub staff
+# So that I can tell them how awesome they are
+} do
+
+# Acceptance Criteria:
+
+# I must specify a valid email address
+# I must specify a subject
+# I must specify a description
+# I must specify a first name
+# I must specify a last name
+	
+	it "should have a contact form" do
+		visit '/contact_forms'
+		expect(page).to have_content("Contact Form")
+	end
+
+	it "should specify validated info" do
+		visit '/contact_forms'
+		fill_in 'email', with: 'doug@example.com'
+		fill_in 'subject', with: 'Hey'
+		fill_in 'description', with: 'What\'s going on this weekend?'
+		fill_in 'first', with: 'Doug'
+		fill_in 'last', with: 'Smith'
+		click_link 'Submit Email'
+	end
+end
