fb883d4e739b216050df6cac7e3c2dec84bf5412
Doug Gerber <dojoh@outlook.com>
2013-09-12 15:48:21 -0400

    Edit End Syntax

--- spec/features/admin_features_spec.rb
+++ spec/features/admin_features_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 feature "Contact Form", %q{
-As an admin
-I want to review contact inquiries
-So that I can respond or take action
+	As an admin
+	I want to review contact inquiries
+	So that I can respond or take action
 } do
 
 # Acceptance Criteria:
@@ -17,23 +17,23 @@ So that I can respond or take action
 		expect(page).to have_content(field.email)
 		expect(page).to have_content(field.message)
 	end
+end
 
 	feature "Contact Form", %q{
-As an admin
-I want to delete an inquiry
-So that I can remove spam or other undesirable inquiries
-} do
+		As an admin
+		I want to delete an inquiry
+		So that I can remove spam or other undesirable inquiries
+	} do
 
 # Acceptance Criteria:
 # I can remove an item from the listing of contact inquiries
 
-		it "should list messages for review" do
-			field = FactoryGirl.create(:contact_form)
-			visit '/contact_forms'
-			expect(page).to have_content(field.name)
-			expect(page).to have_content(field.subject)
-			expect(page).to have_content(field.email)
-			expect(page).to have_content(field.message)
-		end
+	it "should list messages for review" do
+		field = FactoryGirl.create(:contact_form)
+		visit '/contact_forms'
+		expect(page).to have_content(field.name)
+		expect(page).to have_content(field.subject)
+		expect(page).to have_content(field.email)
+		expect(page).to have_content(field.message)
 	end
 end
